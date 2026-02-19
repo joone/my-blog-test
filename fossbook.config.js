a9ab27c63b02256390cc3bf75c7d2d0bf628a86a
Joone Hur <joone@outlook.com>
2026-02-18 23:20:29 -0800

    Initial fossbook site

---
+++ fossbook.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  blogName: "My Blog",
+  authorName: "",
+  authorDescription: "",
+  authorWebsite: "",
+  blogDescription: "A blog powered by fossbook",
+  blogsite: "http://localhost:3000",
+
+  // Optional
+  githubCNAME: "",
+  googleAnalyticsID: "",
+  authorTwitter: "",
+  siteTwitter: "",
+  githubRepository: "",
+  image: "",
+  theme: "archie",
+
+  // Comment system (optional)
+  // comments: { provider: "utterances", repo: "user/repo", issueTerm: "pathname", theme: "github-light" },
+
+  // Deployment
+  // deploy: { branch: "main", remote: "origin" },
+
+  // Directory overrides (defaults shown)
+  content: "./content",
+  postsDir: "./content/posts",
+  outputDir: "./public",
+  staticDir: "./static",
+  themesDir: "./themes",
+};
